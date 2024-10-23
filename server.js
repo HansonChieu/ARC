95ee500b3b0da9e6915eee65caccd11a3aaf9eca
Hanson Chieu <hansonchieu@hotmail.com>
2024-10-23 01:50:00 -0400

    Updated

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const express = require('express'); // "require" the Express module
-const path = require('path');
+const express = require("express"); // "require" the Express module
+const path = require("path");
 const app = express(); // obtain the "app" object
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/home.html'));
-  });
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/home.html"));
+});
 
 const HTTP_PORT = process.env.PORT || 8080; // assign a port
 // start the server on the port and output a confirmation to the console
